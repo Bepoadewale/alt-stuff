36deb5044cb452b0b5d01618b2790e7afc8d2c8f
bepoadewale <bepoadewale@yahoo.com>
2024-12-13 22:00:28 +0100

    c11

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket111"
-    key            = "ecs-deployment/terraform.tfstate"
     region         = var.aws_region
-    dynamodb_table = "terraform-locks"
   }
 }
